442b69ad65de2d074cef54ebe9585007e3ec6d7f
Luiz <lgsf@outlook.com>
2020-05-29 19:34:50 -0300

    Added calendar to first page
    Added vuex

--- ProjectZero/src/main.js
+++ ProjectZero/src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import firebase from "firebase";
+import VueTextareaAutosize from 'vue-textarea-autosize'
+
+Vue.use(VueTextareaAutosize)
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDcPbYSZw_pmc6nQl6MTTZ2LUIXwt6Tu_g",
+  authDomain: "projectzero-9bff2.firebaseapp.com",
+  databaseURL: "https://projectzero-9bff2.firebaseio.com",
+  projectId: "projectzero-9bff2",
+  storageBucket: "projectzero-9bff2.appspot.com",
+  messagingSenderId: "793390803395",
+  appId: "1:793390803395:web:6526a11dff48740bb03576"
+});
+
+export const db = firebase.firestore();
+
 
 new Vue({
   router,
