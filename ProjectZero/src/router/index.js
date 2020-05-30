5cc6b5e2860c246036e0092bd7c6fccabcb75c23
CAMA06 <marco.carmo@viridis.energy>
2020-05-30 18:50:41 -0300

    Listagem e edição de grupos: em andamento...

--- ProjectZero/src/router/index.js
+++ ProjectZero/src/router/index.js
@@ -7,68 +7,75 @@ import Setup from '../views/Setup.vue'
 import Process from '../views/Process.vue'
 import Flux from '../views/Flux.vue'
 import Team from '../views/Team.vue'
+import Groups from '../views/Groups.vue'
 import Notifications from '../views/Notifications.vue'
 
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Login',
     component: Login,
-    
+
   },
-  { 
+  {
     path: '/home', //Home
     name: 'Home',
     component: Home
-   },
-  { 
+  },
+  {
     path: '/setup', //Configuração
     name: 'Setup',
     component: Setup,
     meta: {
-        requiresAuth: true,
-    }},
+      requiresAuth: true,
+    }
+  },
   {
     path: '/process', //Processos
     name: 'Process',
     component: Process,
     meta: {
       requiresAuth: true,
-  }
-    },
+    }
+  },
   {
-  path: '/clients', //Processos
-  name: 'Clients',
-  component: Clients,
-  meta: {
-    requiresAuth: true,
-}
+    path: '/clients', //Processos
+    name: 'Clients',
+    component: Clients,
+    meta: {
+      requiresAuth: true,
+    }
   },
   {
     path: '/flux', //Fluxos
     name: 'Flux',
     component: Flux,
-    },
+  },
   {
     path: '/team', //Usuários
     name: 'Team',
     component: Team,
-    },
+  },
+  {
+    path: '/groups', //Grupos
+    name: 'Groups',
+    component: Groups,
+  },
   {
     path: '/notifications', //Notificações
     name: 'Notifications',
     component: Notifications,
-    }
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-  
+
 })
 
 export default router
