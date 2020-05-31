60af61df300d1e66e50df37b8effa746971538e8
AQRA01 <Raphael@viridis.energy>
2020-05-31 19:12:05 -0300

    tela de usuario

---
+++ ProjectZero/src/views/EditUser.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <h1 class="primary--text">Usuário</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Nome*" :value="name" required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Email" :value="email"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Telefone" :value="phone"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Data de nascimento" :value="birthDate"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-select 
+                  :items="options" 
+                  :label="themeSelectLabel" 
+                  v-model="selectedTheme" 
+                  @change="setSelectedTheme($event)"
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Obrigatório</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close">Fechar</v-btn>
+          <v-btn color="blue darken-1" text @click="close">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+import { db } from "@/main";
+
+var menuItems = [];
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      name: '',
+      email: '',
+      phone: '',
+      birthDate: '',
+      group: '',
+      userGroups: []
+    };
+  },
+  methods: {
+    show() {
+      this.dialog = true
+    },
+    close() {
+      this.name: ''
+      this.email: ''
+      this.phone: ''
+      this.birthDate: ''
+      this.group: ''
+      this.dialog = false
+    }
+  },
+  mounted() {
+  }
+};
+</script>
+<style lang="stylus"></style>
--- ProjectZero/src/views/Users.vue
+++ ProjectZero/src/views/Users.vue
@@ -31,8 +31,8 @@
             </v-col>
           </v-row>
           <v-btn color="red" dark fixed bottom right fab>
-            <v-icon v-show="selected.length == 0" @click="editGroup">mdi-plus</v-icon>
-            <v-icon v-show="selected.length == 1" @click="editGroup">mdi-pen</v-icon>
+            <v-icon v-show="selected.length == 0" @click="editUser">mdi-plus</v-icon>
+            <v-icon v-show="selected.length == 1" @click="editUser">mdi-pen</v-icon>
           </v-btn>
         </v-card>
       </v-col>
@@ -41,6 +41,8 @@
 </template>
 
 <script>
+import { db } from '@/main'
+
 export default {
   data() {
     return {
@@ -53,21 +55,59 @@ export default {
           text: "Nome",
           align: "start",
           value: "name"
+        },
+        {
+          text: "E-mail",
+          align: "start",
+          value: "email"
+        },
+        {
+          text: "Telefone",
+          align: "start",
+          value: "phone"
+        },
+        {
+          text: "Data de nascimento",
+          align: "start",
+          value: "birthDate"
+        },
+        {
+          text: "Grupo",
+          align: "start",
+          value: "group"
         }
       ],
       desserts: [
-        {
-          name: "Admin"
-        },
-        {
-          name: "Operador"
-        }
       ]
     };    
   },
   methods: {
+    readUsers() {
+      db.collection("users")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            console.log(doc)
+            this.desserts.push({
+                id: doc.id,
+                name: doc.data().name,
+                phone: doc.data().phone,
+                email: doc.data().email,
+                birthDate: doc.data().birth_date,
+                group: doc.data().group_name
+                })
+            });
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
+        });
+    },
+    editUser: function() {
+      this.$refs.EditUser.show();
+    }
   },
   mounted() {
+    this.readUsers()
   },
 };
 </script>
