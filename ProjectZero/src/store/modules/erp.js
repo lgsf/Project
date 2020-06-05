37c3801d148c8ad09faded35887f36cbbd166839
Luiz <lgsf@outlook.com>
2020-06-05 09:39:50 -0300

    Initial layout of ERP order view

---
+++ ProjectZero/src/store/modules/erp.js
@@ -0,0 +1,148 @@
+
+import { db } from "@/main";
+
+const state = () => ({
+    label: '',
+    listTitle: "Ordem de Erp",
+    editTitle: 'Erp',
+    selected: undefined,
+    search: '',
+    searchLabel: 'Buscar',
+    header: [
+        {
+            text: "Nome",
+            align: "start",
+            value: "name"
+        },
+        {
+            text: "Tarefas",
+            align: "start",
+            value: "task"
+        },
+        {
+            text: "Usuário",
+            align: "start",
+            value: "user"
+        }
+    ]
+        ,
+    erp: [],
+    editErp: false,
+    editingName: '',
+    editingTask: '',
+    editingUser: ''
+});
+
+const mutations = {
+    selectErp(state, payload) {
+        state.selected = payload
+    },
+    searchFor(state, payload) {
+        state.search = payload
+    },
+    updateErp(state, payload) {
+        state.erp = payload
+    },
+    editErp(state, payload) {
+        let anySelected = !!state.selected;
+        state.editingName = anySelected ? state.selected.name : '';
+        state.editingTask = anySelected ? state.selected.task : '';
+        state.editingUser = anySelected ? state.selected.user : '';
+        state.editErp = payload
+    },
+    editName(state, payload) {
+        state.editingName = payload;
+    },
+    editTask(state, payload) {
+        state.editingTask = payload;
+    },
+    editUser(state, payload) {
+        state.editingUser = payload;
+    }
+};
+
+
+function onGroupsLoaded(context, payload) {
+    let erp = [];
+    payload.forEach(erpSnapShot => {
+        let erpData = erpSnapShot.data();
+        erpData.id = erpSnapShot.id;
+        erp.push(erpData);
+    });
+    context.commit('updateErp', erp);
+}
+
+function createNewErp(state) {
+    return db.collection("erp")
+        .add({
+            name: state.editingName || "",
+            task: state.editingTask || "",
+            user: state.editingUser || ""
+        });
+}
+
+function updateExistingErp(state) {
+    return db.collection("erp")
+        .doc(state.selected.id)
+        .set({
+            name: state.editingName || "",
+            task: state.editingTask || "",
+            user: state.editingUser || ""
+        });
+}
+
+const actions = {
+    selectErp({ state, commit }, payload) {
+        if (!state) console.log('Error, state is undifined.');
+        var selected = !payload ? undefined : payload[0];
+        commit('selectErp', selected)
+    },
+    searchFor({ state, commit }, payload) {
+        if (!state) console.log('Error, state is undifined.');
+        commit('searchFor', payload)
+    },
+    loadErp({ state, commit }) {
+        db.collection("erp")
+            .get()
+            .then(function (snapshots) {
+                onGroupsLoaded({ state, commit }, snapshots)
+            });
+    },
+    editErp({ state, commit }, payload) {
+        if (!state) console.log('Error, state is undifined.');
+        commit('editErp', payload)
+    },
+    editName({ state, commit }, payload) {
+        if (!state) console.log('Error, state is undifined.');
+        commit('editName', payload)
+    },
+    editTask({ state, commit }, payload) {
+        if (!state) console.log('Error, state is undifined.');
+        commit('editTask', payload)
+    },
+    editUser({ state, commit }, payload) {
+        if (!state) console.log('Error, state is undifined.');
+        commit('editUser', payload)
+    },
+    saveErp({ state }) {
+        if (!state.selected)
+            createNewErp(state).then(() => {
+                this.dispatch('erp/loadErp');
+                this.dispatch('erp/editErp');
+            });
+        else
+            updateExistingErp(state).then(() => {
+                this.dispatch('erp/loadErp');
+                this.dispatch('erp/editErp');
+            });
+    }
+};
+const getters = {};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
---
+++ ProjectZero/src/views/EditErp.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row justify="center">
+    <v-dialog :value="dialog" persistent scrollable max-width="600px">
+      <v-card>
+        <v-toolbar class="primary" dark>
+          <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Nome da Erp" :value="name" @input="editName" required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Tarefas" :value="task" @input="editTask" required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Usuários" :value="user" @input="editUser" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Obrigatório</small>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="editErp(false)">Fechar</v-btn>
+          <v-btn color="blue darken-1" text @click="saveErp">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+import { mapState, mapActions } from "vuex";
+
+const computed = mapState("erp", {
+  selected: state => state.selected,
+  title: state => state.editTitle,
+  dialog: state => state.editErp,
+  name: state => state.editingName,
+  task: state => state.editingTask,
+  user: state => state.editingUser
+});
+
+const methods = mapActions("erp", [
+  "editName",
+  "editTask",
+  "editUser",
+  "loadErp",
+  "editErp",
+  "saveErp"
+]);
+
+export default {
+  computed,
+  methods,
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="stylus"></style>
