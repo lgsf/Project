1857c020ea7d5d82a08cc325ab582dfee191fa63
Luiz <lgsf@outlook.com>
2020-07-07 11:23:09 -0300

    Fixing router exception when called in first login and opening of application

--- ProjectZero/src/utilities/permissionRouterGuard.js
+++ ProjectZero/src/utilities/permissionRouterGuard.js
@@ -2,6 +2,9 @@ function hasPermissionsNeeded(to, from, permission) {
     if (permission.some(e => e.route === to.path)) {
         return true
       }
+    else if (from.path === "/" && permission.length === 0) {
+        return true
+    }
     else if (from.path === "/serviceOrder") {
         return true
     }
